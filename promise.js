9d456ba8c93f6735611bafeae2da491d09921c40
muhammad fakhri <12muhammadfakhri@gmail.com>
2022-06-12 08:30:08 +0700

    feat: push tugas ke-7

--- promise.js
+++ promise.js
@@ -1,7 +1,14 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  let x = await promiseTheaterIXX();
+  let y = await promiseTheaterVGC();
+  const nilaibaru = (await x).concat(await y);
+  // return nilaibaru.filter((arr) => arr.hasil === emosi);
+  return nilaibaru.filter((arr) => arr.hasil === emosi).length;
+  // return console.log(arrFilm);
+};
 
 module.exports = {
   promiseOutput,
